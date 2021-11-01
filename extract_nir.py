39e77b2bd3e91523e129d6beba17f6c41f186d48
eduarde <eduard.erja@gmail.com>
2021-11-01 10:27:51 +0200

    Extracted date from file and append to dataframe

--- extract_nir.py
+++ extract_nir.py
@@ -2,14 +2,28 @@ import os
 import sys
 import pandas as pd
 
+
+def get_date_file(filename):
+    try:
+        cols = [8]
+        data = pd.read_excel(filename, usecols=cols)
+        data.head()
+        df = data
+        return df.iloc[5][0]
+    except IndexError:
+        return ""
+
+
 def read_file(filename):
     cols = [2, 3, 4, 5]
     data = pd.read_excel(filename, usecols=cols)
     data.head()
-    df = data
+    df = data 
     df = df.dropna()
     df = df.iloc[2:]
+    df['Date'] = pd.to_datetime(get_date_file(filename)).date()
     return df
+
 
 def write_file(df, filename):
     df.to_excel(filename, sheet_name='Total', index=False)
